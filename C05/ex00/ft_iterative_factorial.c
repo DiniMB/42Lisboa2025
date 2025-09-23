753870c5eabbc8f57df91d974fafd8c00e8e9120
K4p1o <121300541+DiniMB@users.noreply.github.com>
2025-09-23 10:37:53 +0100

    Add files via upload

---
+++ C05/ex00/ft_iterative_factorial.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbaltaza <dbaltaza@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/23 09:40:36 by dbaltaza          #+#    #+#             */
+/*   Updated: 2025/09/23 10:32:35 by dbaltaza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_iterative_factorial(int nb)           // Declara a função que recebe um inteiro nb.
+{
+    int	result;                              // Declara uma variável para armazenar o resultado.
+
+    if (nb < 0)                              // Se nb for negativo...
+        return (0);                          // ...retorna 0 (não existe fatorial de negativo).
+    if (nb == 0)                             // Se nb for zero...
+        return (1);                          // ...retorna 1 (por definição, 0! = 1).
+    result = 1;                              // Inicializa result com 1.
+    while (nb > 1)                           // Enquanto nb for maior que 1...
+    {
+        result *= nb;                        // ...multiplica result por nb.
+        nb--;                                // ...decrementa nb.
+    }
+    return (result);                         // Retorna o resultado final.
+}                                           // Fim da função.
---
+++ C05/ex01/ft_recursive_factorial.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbaltaza <dbaltaza@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/23 09:40:34 by dbaltaza          #+#    #+#             */
+/*   Updated: 2025/09/23 10:32:20 by dbaltaza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int ft_recursive_factorial(int nb)
+{
+	int number;
+	
+	if (nb < 0)
+		return (0);
+	if (nb == 0)
+		return (1);
+	number = 1;
+	while (nb > 1)
+	{
+	//	printf("number -> %d\nnb -> %d\n", number, nb);
+		number *= nb;
+		nb--;
+		ft_recursive_factorial(nb);
+	}
+	return (number); 
+}
+/* int	main(void)
+{
+	printf("%d\n", ft_recursive_factorial(6));
+	return(0);
+} */
