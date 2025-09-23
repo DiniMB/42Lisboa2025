43cf6e39e5d73571060710272ce4f12e5c38709a
K4p1o <121300541+DiniMB@users.noreply.github.com>
2025-09-23 16:36:37 +0100

    Add files via upload

--- C05/ex06/ft_is_prime.c
+++ C05/ex06/ft_is_prime.c
@@ -6,11 +6,22 @@
 /*   By: dbaltaza <dbaltaza@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/23 09:40:25 by dbaltaza          #+#    #+#             */
-/*   Updated: 2025/09/23 15:31:00 by dbaltaza         ###   ########.fr       */
+/*   Updated: 2025/09/23 16:34:13 by dbaltaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int ft_is_prime(int nb)
 {
-	
+    int i;
+
+    if (nb <= 1)
+        return (0); // negativos, 0 e 1 não são primos
+    i = 2;
+    while (i * i <= nb) // só até à raiz quadrada
+    {
+        if (nb % i == 0)
+            return (0); // achou divisor → não é primo
+        i++;
+    }
+    return (1); // nenhum divisor → é primo
 }
--- C05/ex08/ft_ten_queens_puzzle.c
+++ C05/ex08/ft_ten_queens_puzzle.c
@@ -1,12 +1,79 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ten_queens_puzzle.c.c                           :+:      :+:    :+:   */
+/*   ft_ten_queens_puzzle.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dbaltaza <dbaltaza@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/23 09:40:20 by dbaltaza          #+#    #+#             */
-/*   Updated: 2025/09/23 09:40:21 by dbaltaza         ###   ########.fr       */
+/*   Updated: 2025/09/23 16:35:25 by dbaltaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int		is_safe(int board[], int row, int col)
+{
+	int i;
+
+	i = 0;
+	while (i < row)
+	{
+		if (board[i] == col ||                    // mesma coluna
+			board[i] - i == col - row ||          // diagonal principal
+			board[i] + i == col + row)            // diagonal secundária
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	print_solution(int board[])
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		ft_putchar(board[i] + '0'); // imprime colunas como dígitos
+		i++;
+	}
+	ft_putchar('\n');
+}
+
+void	solve(int row, int board[], int *count)
+{
+	int col;
+
+	if (row == 10) // solução encontrada
+	{
+		print_solution(board);
+		(*count)++;
+		return ;
+	}
+	col = 0;
+	while (col < 10)
+	{
+		if (is_safe(board, row, col))
+		{
+			board[row] = col;
+			solve(row + 1, board, count);
+		}
+		col++;
+	}
+}
+
+int		ft_ten_queens_puzzle(void)
+{
+	int board[10];
+	int count;
+
+	count = 0;
+	solve(0, board, &count);
+	return (count);
+}
