bbd2ffc1329d43f35addf17c00abf9c95e3c5f64
K4p1o <121300541+DiniMB@users.noreply.github.com>
2025-09-23 16:30:39 +0100

    Add files via upload

--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -6,33 +6,34 @@
 /*   By: dbaltaza <dbaltaza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/17 11:53:29 by dbaltaza          #+#    #+#             */
-/*   Updated: 2025/09/17 13:27:36 by dbaltaza         ###   ########.fr       */
+/*   Updated: 2025/09/23 16:29:36 by dbaltaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strstr(char *str, char *to_find)
+char	*ft_strstr(char	*str, char *to_find)
 {
-	int i;
-	int tmp;
+	int	i;
+	int	j;
 
-	if (*to_find == '\0')
+	i = 0;
+	if (to_find[0] == '\0')
 		return (str);
-	while (*str != '\0')
+	while (str[i])
 	{
-		if (*str == *to_find)
-		{
-			i = 0;
-			tmp = 0;
-			while (to_find[i] != '\0')
-			{
-				if (str[i] != to_find[i])
-					tmp = 1;
-				i++;
-			}
-			if (tmp == 0)
-				return (str);
-		}
-		str++;
+		j = 0;
+		while (str[i + j] == to_find[j] && to_find[j])
+			j++;
+		if (to_find[j] == '\0')
+			return (&str[i]);
+		i++;
 	}
 	return (0);
 }
+
+/*int main(void)
+{
+	char str[] = "AIJDSNIHANDKUHADSJoaoNDBSUAD";
+	char to_find[] = "Joao";
+	ft_strstr(str, to_find);
+	return (0);
+}*/
