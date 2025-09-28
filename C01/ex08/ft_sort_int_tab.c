162d2744869d82c4ef1018add36c7b5ff1408cfa
K4p1o <121300541+DiniMB@users.noreply.github.com>
2025-09-28 16:41:21 +0100

    Add files via upload

--- C01/ex08/ft_sort_int_tab.c
+++ C01/ex08/ft_sort_int_tab.c
@@ -5,12 +5,35 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dbaltaza <dbaltaza@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/15 14:51:57 by dbaltaza          #+#    #+#             */
-/*   Updated: 2025/09/15 14:52:30 by dbaltaza         ###   ########.fr       */
+/*   Created: 2025/09/28 16:39:14 by dbaltaza          #+#    #+#             */
+/*   Updated: 2025/09/28 16:39:47 by dbaltaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_sort_int_tab(int *tab, int size)
 {
+	int i;
+	int j;
+	int min_idx;
+	int temp;
 	
+	i = 0;
+	while (i < size - 1)
+	{
+		min_idx = i;
+		j = i + 1;
+		while (j < size)
+		{
+			if (tab[j] < tab[min_idx])
+				min_idx = j;
+			j++;
+		}
+		if (min_idx != i)
+		{
+			temp = tab[i];
+			tab[i] = tab[min_idx];
+			tab[min_idx] = temp;
+		}
+		i++;
+	}
 }
