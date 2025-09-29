88b76d8425f6b6b8a45c53795c19f72790f6d8b4
K4p1o <121300541+DiniMB@users.noreply.github.com>
2025-09-29 11:32:53 +0100

    Add files via upload

--- C07/ex03/ft_strjoin.c
+++ C07/ex03/ft_strjoin.c
@@ -5,14 +5,89 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dbaltaza <dbaltaza@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/24 13:11:09 by dbaltaza          #+#    #+#             */
-/*   Updated: 2025/09/24 13:11:32 by dbaltaza         ###   ########.fr       */
+/*   Created: 2025/09/29 11:19:34 by dbaltaza          #+#    #+#             */
+/*   Updated: 2025/09/29 11:31:24 by dbaltaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-char *ft_strjoin(int size, char **strs, char *sep)
+int	ft_strlen(char *str)
 {
-	
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
 }
+
+int	ft_total_size(int size, char **strs, char *sep)
+{
+	int	i;
+	int	sep_len;
+	int	total_len;
+
+	i = 0;
+	sep_len = ft_strlen(sep);
+	total_len = 0;
+	while (i < size)
+	{
+		total_len += ft_strlen(strs[i]);
+		if (i < size - 1)
+			total_len += sep_len;
+		i++;
+	}
+	return (total_len);
+}
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
+
+char	*ft_strjoin(int size, char **strs, char *sep)
+{
+	char	*text;
+	int		i;
+	int		total_len;
+
+	i = 0;
+	total_len = ft_total_size(size, strs, sep) + 1;
+	text = malloc(sizeof(char) * total_len);
+	if (!text)
+		return (0);
+	*text = 0;
+	while (i < size)
+	{
+		ft_strcat(text, strs[i]);
+		if (i < size - 1)
+			ft_strcat(text, sep);
+		i++;
+	}
+	return (text);
+}
+/*
+int	main(void)
+{
+	#include <stdio.h>
+	int	size = 5;
+	char	*strs[] = {"a", "b", "salut", "", "OK"};
+	char	sep[] = "|--|";
+
+	printf("OUTPUT: %s\n", ft_strjoin(size, strs, sep));
+	return (0);
+}*/
